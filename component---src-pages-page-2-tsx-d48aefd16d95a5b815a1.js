ec12af781224470e8933e53e254cb23061a065b0
Thanh Nguyen <908739+seitensei@users.noreply.github.com>
2021-04-10 16:44:02 -0400

    Updates

---
+++ component---src-pages-page-2-tsx-d48aefd16d95a5b815a1.js
@@ -0,0 +1,2 @@
+(self.webpackChunkseitensei_github_io=self.webpackChunkseitensei_github_io||[]).push([[473],{1922:function(e,t,n){"use strict";n.r(t);var l=n(7294),i=n(5444),u=n(9804);t.default=function(){return l.createElement(u.A,null,l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(i.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-d48aefd16d95a5b815a1.js.map
