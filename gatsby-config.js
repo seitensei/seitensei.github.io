6897823a4d6452b5574c5973e0bb575a03d57432
Thanh Nguyen <908739+seitensei@users.noreply.github.com>
2021-04-11 15:39:38 -0400

    comment unused config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,27 +8,27 @@ module.exports = {
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-jss`,
         `gatsby-plugin-image`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
+        // {
+        //     resolve: `gatsby-source-filesystem`,
+        //     options: {
+        //         name: `images`,
+        //         path: `${__dirname}/src/images`,
+        //     },
+        // },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-            },
-        },
+        // {
+        //     resolve: `gatsby-plugin-manifest`,
+        //     options: {
+        //         name: `seitensei.github.io`,
+        //         short_name: `seitensei`,
+        //         start_url: `/`,
+        //         background_color: `#663399`,
+        //         theme_color: `#663399`,
+        //         display: `minimal-ui`,
+        //         icon: ``, // This path is relative to the root of the site.
+        //     },
+        // },
         `gatsby-transformer-remark`,
         //`gatsby-plugin-gatsby-cloud`,
         // this (optional) plugin enables Progressive Web App + Offline functionality
