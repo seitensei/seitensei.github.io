e119d0c1a9561aa559a2a25f2812b9d58d0dee06
Thanh Nguyen <908739+seitensei@users.noreply.github.com>
2021-04-14 09:10:47 -0400

    change ssr/browser function

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-import '@fontsource/roboto';
-
 import GlobalStateProvider from './src/components/globalStateProvider/globalStateProvider';
 
 exports.wrapRootElement = ({ element, props }) => {
