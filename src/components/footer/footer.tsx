ad93437f73cf9ee846be65eba66b475756da7697
Thanh Nguyen <908739+seitensei@users.noreply.github.com>
2021-04-27 11:42:49 -0400

    add twitter icon

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,6 +4,7 @@ import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
 import clsx from 'clsx';
 import React from 'react';
 
@@ -66,6 +67,14 @@ export const Footer = (props: IFooterProps) => {
                                     rel="noopener"
                                 >
                                     <LinkedInIcon />
+                                </IconButton>
+                                <IconButton
+                                    aria-label="Twitter"
+                                    href="https://twitter.com/tnsicdr"
+                                    target="_blank"
+                                    rel="noopener"
+                                >
+                                    <TwitterIcon />
                                 </IconButton>
                             </Grid>
                             <Grid item xs={12}>
