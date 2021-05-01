ac8d34886aaa323ee2b5b80beb77e0a4a49febf2
Thanh Nguyen <908739+seitensei@users.noreply.github.com>
2021-05-01 17:55:34 -0400

    Updates

---
+++ component---src-pages-404-tsx-0b1f6b9d253d43bb3c4a.js
@@ -0,0 +1,2 @@
+(self.webpackChunkseitensei_github_io=self.webpackChunkseitensei_github_io||[]).push([[218],{9786:function(e,t,n){"use strict";n.r(t);var u=n(7294),s=n(4415);t.default=function(){return u.createElement(s.A,null,u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-0b1f6b9d253d43bb3c4a.js.map
