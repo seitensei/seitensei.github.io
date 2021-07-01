99a1c5de7ca544050993e88c690f3b82cab7ca6d
Thanh Nguyen <908739+tnsicdr@users.noreply.github.com>
2021-07-01 01:52:49 -0400

    add styledcomponents

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,47 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
-  siteMetadata: {
-    title: "code.tnsi",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-wordpress",
-      options: {
-        url: process.env.WPGRAPHQL_URL,
-      },
+    siteMetadata: {
+        title: 'code.tnsi',
     },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    //"gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+    plugins: [
+        {
+            resolve: 'gatsby-source-wordpress',
+            options: {
+                url: process.env.WPGRAPHQL_URL,
+            },
+        },
+        'gatsby-plugin-styled-components',
+        'gatsby-plugin-sass',
+        'gatsby-plugin-image',
+        'gatsby-plugin-react-helmet',
+        //"gatsby-plugin-sitemap",
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/icon.png',
+            },
+        },
+        'gatsby-plugin-mdx',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+    ],
 };
